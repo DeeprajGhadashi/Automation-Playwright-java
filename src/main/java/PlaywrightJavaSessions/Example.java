f7991b9cd1ff9b5809a0869823d8119204ef5daf
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-15 17:47:21 +0530

    Playwright Inspector is open 1) Recording a test code using codegrn and 2) Debugging using page.pause() and Run Configuration Name:PWDEBUG & Value:1

---
+++ src/main/java/PlaywrightJavaSessions/Example.java
@@ -0,0 +1,34 @@
+package PlaywrightJavaSessions;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
+
+//Recording a test using running codegen and Debugging
+public class Example {
+	public static void main(String[] args) {
+	    try (Playwright playwright = Playwright.create()) {
+	      Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+	        .setHeadless(false));
+	      BrowserContext context = browser.newContext();
+	      Page page = context.newPage();
+	      page.navigate("https://academy.naveenautomationlabs.com/");
+	      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Login")).click();
+	      page.frameLocator("#microfe-popup-login").getByText("Sign up").click();
+	      page.pause();
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Name").click();
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Name").fill("Deepraj Santosh Ghadashi");
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Email address").click();
+	    
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Email address").fill("deep@gmail.com");
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Password").click();
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Password").fill("Deep@12");
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Enter your number").click();
+	      page.frameLocator("#microfe-popup-login").getByPlaceholder("Enter your number").fill("+91 77743-958311");
+	      page.frameLocator("#microfe-popup-login").locator("#loginPopupCloseBtn path").click();
+	    }
+	  }
+}
