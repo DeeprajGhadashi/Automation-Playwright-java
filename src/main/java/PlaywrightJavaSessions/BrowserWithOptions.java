2f568d6dd1db7e5a038e0d08ac4256306fc3eab7
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-15 13:29:41 +0530

    Browsers with Chromium options

---
+++ src/main/java/PlaywrightJavaSessions/BrowserWithOptions.java
@@ -0,0 +1,34 @@
+package PlaywrightJavaSessions;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class BrowserWithOptions {
+
+	public static void main(String[] args) {
+		
+        Playwright playwright = Playwright.create();
+		
+        LaunchOptions lp= new LaunchOptions();
+        lp.setChannel("chrome");  //msedge
+        lp.setHeadless(false);
+        
+       Browser browser = playwright.chromium().launch(lp);
+        
+		Page page = browser.newPage();
+		page.navigate("https://www.amazon.com");
+		
+		String title = page.title();
+		System.out.println("page title is : " + title);
+		
+		String url = page.url();
+		System.out.println("url is " + url);
+		
+		browser.close();
+		playwright.close();
+	}
+
+}
