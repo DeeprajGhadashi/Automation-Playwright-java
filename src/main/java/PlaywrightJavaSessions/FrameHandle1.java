3eb2c0e6fdf288f4c6d91983c2af29d70ff4f96c
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-19 22:19:00 +0530

    handle iframe in playwright with java

--- src/main/java/PlaywrightJavaSessions/FrameHandle1.java
+++ src/main/java/PlaywrightJavaSessions/FrameHandle1.java
@@ -6,27 +6,21 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
-public class FrameHandle {
+public class FrameHandle1 {
 
 	public static void main(String[] args) {
+	
 		Playwright playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 		
 
 		BrowserContext browserContext = browser.newContext();
 		Page page = browserContext.newPage();
-		page.navigate("https://londonfreelance.org/courses/frames/index.html");
+		page.navigate("https://www.formsite.com/templates/registration-from-templates/vehicle-registration-form/");
 		
-		//frame
-		//approach1 used Xpath
-		String header = page.frameLocator("//frame[@name='main']").locator("h2").textContent();
-		System.out.println(header);
+		page.locator("img[title='Vehicle-Registration-Forms-and-Examples']").click();
 		
-		//approach2
-		String h2 = page.frame("main").locator("h2").textContent();
-		System.out.println(h2);
-			
-		
+		page.frameLocator("//iframe[contains(@id,'frame-one')]").locator("#RESULT_TextField-8").fill("Deepraj");
 	}
 
 }
