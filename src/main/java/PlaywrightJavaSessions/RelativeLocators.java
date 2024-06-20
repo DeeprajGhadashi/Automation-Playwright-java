da7d2d3ef19f06c5cccb94562a9d00cb6e5a6fd9
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-20 13:34:23 +0530

    Relative CSS Slectors in Playwright

---
+++ src/main/java/PlaywrightJavaSessions/RelativeLocators.java
@@ -0,0 +1,33 @@
+package PlaywrightJavaSessions;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class RelativeLocators {
+
+	static Page page;
+	public static void main(String[] args) {
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		
+		BrowserContext browserContext = browser.newContext();
+		page = browserContext.newPage();  // Initialize the static page variable
+		page.navigate("https://selectorshub.com/xpath-practice-page/");
+		
+		//page.locator("input[type='checkbox']:left-of(:text('Joe.Root'))").first().click();
+		
+		selectUser("Joe.Root");
+		selectUser("Garry.white");
+		selectUser("Jordan.Mathews");
+		
+		String userRole = page.locator("td:right-of(:text('Joe.Root'))").first().textContent();
+		System.out.println(userRole);
+	}
+	
+	public static void selectUser(String userName) {
+		page.locator("input[type='checkbox']:left-of(:text('" + userName + "'))").first().click();
+	}
+}
