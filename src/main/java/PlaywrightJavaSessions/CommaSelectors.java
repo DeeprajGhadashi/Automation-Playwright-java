4aedac605ea720885aa99b3260cffd3f4f862b26
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-20 12:49:48 +0530

    comma seprated css Selector List count

--- src/main/java/PlaywrightJavaSessions/CommaSelectors.java
+++ src/main/java/PlaywrightJavaSessions/CommaSelectors.java
@@ -3,6 +3,7 @@ package PlaywrightJavaSessions;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
@@ -17,12 +18,27 @@ public class CommaSelectors {
 		Page page = browserContext.newPage();
 		page.navigate("https://academy.naveenautomationlabs.com");
 		
-		//comma seprated css List
-		page.locator("a:has-text('SignIn'),"
-				   + "a:has-text('LogIn'),"
-				   + "a:has-text('Signin'),"
-				   + "a:has-text('Login')").click();	
+		//comma seprated css Selector List
+//		page.locator("a:has-text('SignIn'),"
+//				   + "a:has-text('LogIn'),"
+//				   + "a:has-text('Signin'),"
+//				   + "a:has-text('Login')").click();
+//		
 		
+		Locator imp_ele = page.locator("a:has-text('Courses'),"
+				                     + "a:has-text('Demo Site'),"
+				                     + "a:has-text('WebDriver APIs'),"
+				                     + "a:has-text('Webinars'),"
+				                     + "a:has-text('Login'),");
+				
+		System.out.println(imp_ele.count());
+		
+		if(imp_ele.count() == 5) {
+			System.out.println("PASS");
+		}
+		else {
+			System.out.println("FAIL");
+		}
 
 	}
 
